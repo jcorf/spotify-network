eaa509a9081b8da4654eedf3201161625c40b839
julie <corfman.j@northeastern.edu>
2021-02-21 03:34:46 -0500

    redirect

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "74074a35e175406aa61bf5f33b3d0c14";
-export const redirectUri = "http://localhost:3000";
-//export const redirectUri = "https://jcorf.github.io/spotify-network/";
+// export const redirectUri = "http://localhost:3000";
+export const redirectUri = "https://jcorf.github.io/spotify-network/";
 export const scopes = [
   "user-top-read",
   "user-read-recently-played"
